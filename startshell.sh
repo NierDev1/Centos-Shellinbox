9419a3906442de30d691936ac660677fdf837832
nierdev <158775592+nierdev@users.noreply.github.com>
2024-02-05 13:53:36 +0800

    Update startshell.sh

--- startshell.sh
+++ startshell.sh
@@ -1,5 +1,5 @@
 # Este comando sempre entra como user (nao tem login)
-shellinaboxd --no-beep -t --user-css Normal:+/usr/share/shellinabox/black-on-white.css,Reverse:-/usr/share/shellinabox/white-on-black.css --service "/:root:users:/home/user:/bin/bash"
+shellinaboxd --no-beep -t --user-css Normal:+/usr/share/shellinabox/black-on-white.css,Reverse:-/usr/share/shellinabox/white-on-black.css --service "/:root:users:/bin/bash"
 # Este tem login
 # shellinaboxd --no-beep -t -s "/:LOGIN"
 
