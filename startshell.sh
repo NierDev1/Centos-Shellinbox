6b34dc8579bfa04ff9ab1d8bad740e0f5a58889c
nierdev <158775592+nierdev@users.noreply.github.com>
2024-02-05 13:51:37 +0800

    Update startshell.sh

--- startshell.sh
+++ startshell.sh
@@ -1,15 +1,5 @@
-echo "Criando group docker com gid=$GID"
-groupadd -g $GID docker
-usermod -a -G docker user
-if [ -z $USERPWD ]
-then
-  echo "User default password in use"
-else
-  echo "Changing user password"
-  echo $USERPWD | passwd user --stdin
-fi
 # Este comando sempre entra como user (nao tem login)
-# shellinaboxd --no-beep -t --user-css Normal:+/usr/share/shellinabox/black-on-white.css,Reverse:-/usr/share/shellinabox/white-on-black.css --service "/:user:users:/home/user:/bin/bash"
+shellinaboxd --no-beep -t --user-css Normal:+/usr/share/shellinabox/black-on-white.css,Reverse:-/usr/share/shellinabox/white-on-black.css --service "/:root:users:/home/user:/bin/bash"
 # Este tem login
-shellinaboxd --no-beep -t -s "/:LOGIN"
+# shellinaboxd --no-beep -t -s "/:LOGIN"
 
